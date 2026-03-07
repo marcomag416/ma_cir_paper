9391fabd6a07011924b9e2769fa5af4f3bee2c85
Marco Magnanini <marcomag416@gmail.com>
2026-03-07 18:37:24 +0100

    minor: update CUDA info

--- test_cuda.py
+++ test_cuda.py
@@ -3,8 +3,13 @@ import torch
 if torch.cuda.is_available():
     device = torch.device("cuda")
     print("CUDA is available. Using GPU.")
+    # print(f"GPU Name: {torch.cuda.get_device_name(0)}")
+    print(f"Number of GPUs: {torch.cuda.device_count()}")
+    for i in range(torch.cuda.device_count()):
+        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+    print(f"Available GPU Memory: {torch.cuda.get_device_properties(0).total_memory / (1024 ** 3):.2f} GB")
 else:
     device = torch.device("cpu")
     print("CUDA is not available. Using CPU.")
 
-print(torch.version.cuda)
+print(f"CUDA Version: {torch.version.cuda}")
